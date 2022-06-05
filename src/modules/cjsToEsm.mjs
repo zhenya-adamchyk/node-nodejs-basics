50508204eb1d3fcdc53d90174cdf27d743699886
zhenya_adamchyk <e.adamchyk@softteco.com>
2022-06-05 21:32:11 +0300

    refactor modules

--- src/modules/cjsToEsm.mjs
+++ src/modules/cjsToEsm.mjs
@@ -1,27 +1,31 @@
-import { sep } from 'path';
+import path, { dirname } from 'path'
 import { release, version } from 'os';
 import { createServer as createServerHttp } from 'http';
-import * as a from './files/a.json';
-import * as b from './files/b.json';
-import './files/c';
+import './files/c.mjs';
+import { fileURLToPath } from 'url';
 
 const random = Math.random();
 
-const unknownObject = random > 0.5 ? a : b;
+let unknownObject;
+
+if (random > 0.5) {
+    unknownObject = await import('./files/a.json', { assert: { type: 'json' } });
+} else {
+    unknownObject = await import('./files/b.json', { assert: { type: 'json' } });
+}
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
-console.log(`Path segment separator is "${sep}"`);
+console.log(`Path segment separator is "${path.sep}"`);
 
-console.log(`Path to current file is ${__filename}`);
-console.log(`Path to current directory is ${__dirname}`);
+console.log(`Path to current file is ${fileURLToPath(import.meta.url)}`);
+console.log(`Path to current directory is ${dirname(fileURLToPath(import.meta.url))}`);
 
 const createMyServer = createServerHttp((_, res) => {
     res.end('Request accepted');
 });
 
-export default {
+export {
     unknownObject,
     createMyServer,
 };
-
